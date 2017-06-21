deed7166c9413626b532bfdcaaa303cfa08c11f7
Cheng Zhao <zcbenz@gmail.com>
2017-06-21 19:56:28 +0900

    Initial commit

---
+++ install.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+// Copyright 2017 Cheng Zhao. All rights reserved.
+// Use of this source code is governed by the license that can be found in the
+// LICENSE file.
+
+const downloadYue = require('download-yue')
+
+// Version to download.
+// const {version} = 'v' + require('./package.json')
+const version = 'v0.1.0'
+
+// Which binary to download.
+let runtime = process.env.npm_config_runtime
+if (!runtime)
+  runtime = 'node'
+let nodever = process.env.npm_config_target
+if (!nodever)
+  nodever = process.version
+let targetCpu = process.env.npm_config_arch
+if (!targetCpu)
+  targetCpu = process.arch
+if (targetCpu == 'ia32')
+  targetCpu = 'x86'
+let shortver = nodever.substring(1, nodever.lastIndexOf('.'))
+if (runtime == 'node')
+  shortver = shortver.substring(0, shortver.lastIndexOf('.'))
+const targetOs = {
+  win32: 'win',
+  linux: 'linux',
+  darwin: 'mac',
+}[process.platform]
+const filename = `node_yue_${runtime}_${shortver}_${version}_${targetOs}_${targetCpu}.zip`
+
+// Personal github token.
+const token = process.env.GITHUB_TOKEN
+
+downloadYue(version, filename, __dirname, token).catch((e) => {
+  console.error('Failed to download yue:', e.message)
+  process.exit(1)
+})
