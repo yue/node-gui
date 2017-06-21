932f53e3a672415be1886f7b1982a674ddf47f22
Cheng Zhao <zcbenz@gmail.com>
2017-06-21 21:47:52 +0900

    Fix warning

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (!process.versions.electron || !process.versions.yode)
+if (!process.versions.electron && !process.versions.yode)
   console.warn("Using this module under Node.js is unstable.")
 
 module.exports = require('./gui.node')
